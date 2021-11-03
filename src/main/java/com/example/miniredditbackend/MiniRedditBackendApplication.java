68e211e8db72e398e48303fbd96ce751a83b1349
unknown <marcus.larsen.swe@gmail.com>
2021-11-03 23:41:17 +0100

    git test

---
+++ src/main/java/com/example/miniredditbackend/MiniRedditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniredditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRedditBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniRedditBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniredditbackend/MiniRedditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniredditbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedditBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
